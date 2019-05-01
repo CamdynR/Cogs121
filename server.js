97063d9b0b810d5220d9cac356229ad0634b1c18
Chris Wynne <cwynne@ucsd.edu>
2019-04-30 21:44:03 -0700

    started ajax

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const app = express();
 
 app.use(express.static('static_files'));
 
+const authorDatabase = {
+  'Chris': {stance: 'left', photo: 'Imgaes/chris.jpg'},
+  'Alan': {stance: 'mid',   photo: 'Images/alan.jpg'},
+  'Shreya': {stance: 'right',  photo: 'Images/shreya.jpg'}
+};
+
+app.get('/authors/:authorid', (req, res) => {
+  const nameToLookup = req.params.authorid; // matches ':userid' above
+  const val = authorDatabase[nameToLookup];
+  console.log(nameToLookup, '->', val); // for debugging
+  if (val) {
+    res.send(val);
+  } else {
+    res.send({}); // failed, so return an empty object instead of undefined
+  }
+});
+
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000/');
 });
